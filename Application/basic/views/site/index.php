f8792c08bfdf0790e5d5239c83be8b968bc3d7b8
Argel Janfred Cornel <argeljanfred10@gmail.com>
2017-07-31 10:12:23 +0800

    commit basic yii

--- Application/basic/views/site/index.php
+++ Application/basic/views/site/index.php
@@ -7,9 +7,9 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>HOTEL 2 HOME INSPECTION</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Welcome</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
@@ -18,12 +18,9 @@ $this->title = 'My Yii Application';
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>1.0   Introduction</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Shoe Mart (SM) Hotels was established to address the vast potential of tourism in the Philippines. Its vision and mission is to build world-class businesses that are catalysts for development in the communities they serve and partner with their host communities to provide a consistently high standard of service to their customers, look after the welfare of their employees and deliver sustainable returns to their shareholders, at all times upholding the highest standards of corporate governance in all their businesses. </p>
 
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>

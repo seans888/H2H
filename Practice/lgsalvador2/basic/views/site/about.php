1639351924396c782cada6bc05435c0a9270ff58
leonardo salvador <salvador.leonardo444@gmail.com>
2017-07-31 11:16:58 +0800

    commit about us

---
+++ Practice/lgsalvador2/basic/views/site/about.php
@@ -0,0 +1,78 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Facility Inspection Module';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>About Us</h1>
+
+        <p class="lead">SM Hotels Facility Inspection Module</p>
+
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-3">
+                <h2>Gabriel Estabillo</h2>
+				<h4>Project Manager</h4>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="
+						https://www.facebook.com/gab.estabillo">Facebook &raquo;</a>
+					<a class="btn btn-default" href="
+						https://www.facebook.com/gab.estabillo">Facebook &raquo;</a>
+            </div>
+            <div class="col-lg-3">
+                <h2>Argel Cornel</h2>
+				<h4>Project Documentation</h4>
+				
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="
+						https://www.facebook.com/gab.estabillo">Facebook &raquo;</a>
+					<a class="btn btn-default" href="
+						https://www.facebook.com/gab.estabillo">Facebook &raquo;</a></p>
+            </div>
+            <div class="col-lg-3">
+                <h2>Leo Pogi Salvador</h2>
+				<h4>Project Analyst</h4>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="
+						https://www.facebook.com/gab.estabillo">Facebook &raquo;</a>
+					<a class="btn btn-default" href="
+						https://www.facebook.com/gab.estabillo">Facebook &raquo;</a></p>
+            </div>
+			  <div class="col-lg-3">
+                <h2>Wesley Silvestre</h2>
+				<h4>Project Coder</h4>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="
+						https://www.facebook.com/gab.estabillo">Facebook &raquo;</a>
+					<a class="btn btn-default" href="
+						https://www.facebook.com/gab.estabillo">Facebook &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>

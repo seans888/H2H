d1dc236b6abf79544a85bb1ddc960d7bcc78a6cf
Argel Janfred Cornel <argeljanfred10@gmail.com>
2017-08-16 11:24:22 +0800

    commit

---
+++ Practice/argeljanfred10/basic/views/site/index.php
@@ -0,0 +1,48 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+
+        <h1>Hotel 2 Home Application</h1>
+
+
+        <p class="lead">HEYYYYY</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Client Background</h2>
+
+                <p>Shoe Mart (SM) Hotels was established to address the vast potential of tourism in the Philippines. Its vision and mission is to build world-class businesses that are catalysts for development in the communities they serve and partner with their host communities to provide a consistently high standard of service to their customers, look after the welfare of their employees and deliver sustainable returns to their shareholders, at all times upholding the highest standards of corporate governance in all their businesses. </p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Project Context</h2>
+
+                <p>Inspection, maintenance, repair and other services are key to a smooth and efficient flow of processes in a hotel. Having a high-quality equipment is no guarantee of optimum functionality and efficient processes. It is just as significant to have a new product properly installed and assembled as to have your products inspected and to get repairs or maintenance activities done at the right time. </p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Purpose and Description</h2>
+
+                <p>
+The main purpose of inspection is to provide quality assurance, condition of the systems and components of every facilities of the hotel that are defective or insufficient and need immediate attention, that could have a deleterious effect on the building and its occupants or economic value or marketability of the property. Conditions may be safety concerns, damage, insufficiency or deterioration of hotel components and facilities, anticipated problem due to existing defects or age of hotel component and equipment.
+</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>

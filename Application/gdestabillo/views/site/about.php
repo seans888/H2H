34c19deb3086ed6291d9ab23912ffe526d920dbb
gabestabillo <tetotcommander@gmail.com>
2017-08-16 11:10:41 +0800

    Put directory path for gab image

--- Application/gdestabillo/views/site/about.php
+++ Application/gdestabillo/views/site/about.php
@@ -17,7 +17,7 @@ $this->title = 'Facility Inspection Module';
 
         <div class="row">
             <div class="col-lg-3">
-			<img src="\\Picture\gab.jpg" width="125" height="125" title="Logo of a company" alt="Logo of a company" />
+			<img src="\facility\Picture\gab.jpg" width="125" height="125" title="Logo of a company" alt="Logo of a company" />
 
                 <h2>Gabriel Estabillo</h2>
 				<h4>Project Manager</h4>

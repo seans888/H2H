f120233b1cee7cc1febbf24fb259ea9eca37b11e
Wesley Silvestre <silvestrewes@gmail.com>
2017-08-17 11:55:51 +0800

    Updated the Model and Crud Table.

--- Application/wcsilvestre/views/checklist-template/index.php
+++ Application/wcsilvestre/views/checklist-template/index.php
@@ -24,7 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            // 'id',
+            'id',
             'checklist_template_type',
             'checklist_template_equipment',
             'checklist_template_temperature',

b56b43cc4b4af9c8a1779964da362053b29f1cf1
Wesley Silvestre <silvestrewes@gmail.com>
2017-08-17 11:56:08 +0800

    Updated the Model and Crud Table.

--- Application/wcsilvestre/views/facility/_search.php
+++ Application/wcsilvestre/views/facility/_search.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\FailitySearch */
+/* @var $model app\models\FacilitySearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 

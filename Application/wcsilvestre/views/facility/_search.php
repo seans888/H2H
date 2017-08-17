111cc2319195edfd83cb9580c990d611baf25f7e
Wesley Silvestre <silvestrewes@gmail.com>
2017-08-17 11:38:46 +0800

    Updated the CRUD for Facility table.

--- Application/wcsilvestre/views/facility/_search.php
+++ Application/wcsilvestre/views/facility/_search.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\FacilitySearch */
+/* @var $model app\models\FailitySearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 

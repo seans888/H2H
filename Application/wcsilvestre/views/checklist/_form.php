51f74269cd255d6211c94157eecf672d79f0ed9c
Wesley Silvestre <silvestrewes@gmail.com>
2017-08-17 11:56:00 +0800

    Updated the Model and Crud Table.

--- Application/wcsilvestre/views/checklist/_form.php
+++ Application/wcsilvestre/views/checklist/_form.php
@@ -12,9 +12,11 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
+    <?= $form->field($model, 'id')->textInput() ?>
+
     <?= $form->field($model, 'checklist_equipment')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'checklist_quantity_on_hand')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'checklist_quantity_on_hand')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>

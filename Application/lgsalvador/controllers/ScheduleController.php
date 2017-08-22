7cb3181f19a18ee7443b0cfcf6993700ed00a907
leonardo salvador <salvador.leonardo444@gmail.com>
2017-08-22 10:48:00 +0800

    Added and modified the Controllers.

--- Application/lgsalvador/controllers/ScheduleController.php
+++ Application/lgsalvador/controllers/ScheduleController.php
@@ -3,14 +3,14 @@
 namespace app\controllers;
 
 use Yii;
-use app\models\schedule;
+use app\models\Schedule;
 use app\models\ScheduleSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 
 /**
- * ScheduleController implements the CRUD actions for schedule model.
+ * ScheduleController implements the CRUD actions for Schedule model.
  */
 class ScheduleController extends Controller
 {
@@ -30,7 +30,7 @@ class ScheduleController extends Controller
     }
 
     /**
-     * Lists all schedule models.
+     * Lists all Schedule models.
      * @return mixed
      */
     public function actionIndex()
@@ -45,7 +45,7 @@ class ScheduleController extends Controller
     }
 
     /**
-     * Displays a single schedule model.
+     * Displays a single Schedule model.
      * @param integer $id
      * @return mixed
      */
@@ -57,13 +57,13 @@ class ScheduleController extends Controller
     }
 
     /**
-     * Creates a new schedule model.
+     * Creates a new Schedule model.
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
      */
     public function actionCreate()
     {
-        $model = new schedule();
+        $model = new Schedule();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
@@ -75,7 +75,7 @@ class ScheduleController extends Controller
     }
 
     /**
-     * Updates an existing schedule model.
+     * Updates an existing Schedule model.
      * If update is successful, the browser will be redirected to the 'view' page.
      * @param integer $id
      * @return mixed
@@ -94,7 +94,7 @@ class ScheduleController extends Controller
     }
 
     /**
-     * Deletes an existing schedule model.
+     * Deletes an existing Schedule model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
      * @param integer $id
      * @return mixed
@@ -107,15 +107,15 @@ class ScheduleController extends Controller
     }
 
     /**
-     * Finds the schedule model based on its primary key value.
+     * Finds the Schedule model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
      * @param integer $id
-     * @return schedule the loaded model
+     * @return Schedule the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
     protected function findModel($id)
     {
-        if (($model = schedule::findOne($id)) !== null) {
+        if (($model = Schedule::findOne($id)) !== null) {
             return $model;
         } else {
             throw new NotFoundHttpException('The requested page does not exist.');

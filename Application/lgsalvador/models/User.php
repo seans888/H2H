f5a5ddb012768a572711e15a381ce55945272821
leonardo salvador <salvador.leonardo444@gmail.com>
2017-08-22 10:45:10 +0800

    Added the latest Models in my folder.

---
+++ Application/lgsalvador/models/User.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property integer $id
+ * @property string $firstname
+ * @property string $lastname
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['firstname', 'lastname'], 'required'],
+            [['firstname', 'lastname'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'firstname' => 'Firstname',
+            'lastname' => 'Lastname',
+        ];
+    }
+}

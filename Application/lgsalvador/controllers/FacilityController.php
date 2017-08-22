1c718f6cfcba6eaa48524c669b1c78a21d62ee6c
leonardo salvador <salvador.leonardo444@gmail.com>
2017-08-22 10:48:52 +0800

    Added and modified the Controllers.

---
+++ Application/lgsalvador/controllers/FacilityController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\Facility;
+use app\models\FacilitySearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * FacilityController implements the CRUD actions for Facility model.
+ */
+class FacilityController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Facility models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new FacilitySearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Facility model.
+     * @param integer $id
+     * @param integer $checklist_id
+     * @return mixed
+     */
+    public function actionView($id, $checklist_id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id, $checklist_id),
+        ]);
+    }
+
+    /**
+     * Creates a new Facility model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Facility();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id, 'checklist_id' => $model->checklist_id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing Facility model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @param integer $checklist_id
+     * @return mixed
+     */
+    public function actionUpdate($id, $checklist_id)
+    {
+        $model = $this->findModel($id, $checklist_id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id, 'checklist_id' => $model->checklist_id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Facility model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @param integer $checklist_id
+     * @return mixed
+     */
+    public function actionDelete($id, $checklist_id)
+    {
+        $this->findModel($id, $checklist_id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Facility model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @param integer $checklist_id
+     * @return Facility the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id, $checklist_id)
+    {
+        if (($model = Facility::findOne(['id' => $id, 'checklist_id' => $checklist_id])) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}

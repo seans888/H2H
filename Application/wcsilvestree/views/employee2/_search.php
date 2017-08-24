e339b824fdb284c195435b824465ec4160c501e5
Wesley Silvestre <silvestrewes@gmail.com>
2017-08-24 12:39:55 +0800

    Updated the CRUD and Model after updating the master DB.

---
+++ Application/wcsilvestree/views/employee2/_search.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Employee2Search */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="employee2-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'employee_lastname') ?>
+
+    <?= $form->field($model, 'employee_firstname') ?>
+
+    <?= $form->field($model, 'employee_number') ?>
+
+    <?= $form->field($model, 'employee_email') ?>
+
+    <?php // echo $form->field($model, 'employee_occupation') ?>
+
+    <?php // echo $form->field($model, 'employee_user_type') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>

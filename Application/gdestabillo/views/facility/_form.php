a190258a434d2cd7837e890d76e09506a32589fe
gabestabillo <tetotcommander@gmail.com>
2017-08-30 17:07:39 +0800

    Finalized Yii2.

--- Application/gdestabillo/views/facility/_form.php
+++ Application/gdestabillo/views/facility/_form.php
@@ -2,9 +2,11 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\Checklist;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\facility */
+/* @var $model app\models\Facility */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
@@ -19,6 +21,11 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'facility_qrcode')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'checklist_id')->textInput() ?>
+    <?= $form->field($model, 'checklist_id')->dropDownList(
+        ArrayHelper::map(Checklist::find()->all(), 'id', 'id'),
+        ['prompt'=>'Select Checklist']
+    ) ?>
+
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>

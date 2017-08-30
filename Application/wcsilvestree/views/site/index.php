d19ff65fe86b316fe6d52042dc13f9a08ffae501
alcornel <argeljanfred10@gmail.com>
2017-08-31 00:41:38 +0800

    Finalized Application/wcsilvestree/views/site/index.php

---
+++ Application/wcsilvestree/views/site/index.php
@@ -0,0 +1,96 @@
+<?php
+
+$this->title = 'H2H Application';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <title>Parallax Template - Materialize</title>
+
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+<nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+        <a id="logo-container" href="#" class="brand-logo">Logo</a>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="#">Navbar Link</a></li>
+        </ul>
+
+        <ul id="nav-mobile" class="side-nav">
+            <li><a href="#">Navbar Link</a></li>
+        </ul>
+        <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+</nav>
+
+<div id="index" class="parallax-container">
+    <div class="section no-pad-bot">
+        <div class="container">
+            <br><br>
+            <h1 class="header center teal-text text-lighten-2">Hotel 2 Home Application</h1>
+            <br><br>
+
+        </div>
+    </div>
+    <div class="parallax"><img src="logo.png" alt="Unsplashed background img 1"></div>
+</div>
+
+
+<div class="container">
+    <div class="section">
+
+        <!--   Icon Section   -->
+        <div class="row">
+            <div class="col s12 m4">
+                <div class="icon-block">
+                    <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
+                    <h5 class="center">Client</h5>
+
+                    <p class="light">Shoe Mart (SM) Hotels was established to address the vast potential of tourism in the Philippines. Its vision and mission is to build world-class businesses for their clients.
+                        </p>
+                </div>
+            </div>
+
+            <div class="col s12 m4">
+                <div class="icon-block">
+                    <h2 class="center brown-text"><i class="material-icons">group</i></h2>
+                    <h5 class="center">Project Context</h5>
+
+                    <p class="light">The Inspection Application enables hoteliers to manage all inspections from a smartphone. Once the
+                        inspection process is done, the app will automatically mail the data to the management. </p>
+                </div>
+            </div>
+
+            <div class="col s12 m4">
+                <div class="icon-block">
+                    <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
+                    <h5 class="center">Purpose and Description</h5>
+
+                    <p class="light">The main purpose of inspection is to provide quality assurance, condition of the systems and components
+                        of every facilities of the hotel that are defective or insufficient and need immediate attention.
+                       </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+
+
+<!--  Scripts-->
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="js/materialize.js"></script>
+<script src="js/init.js"></script>
+
+</body>
+</html>
+

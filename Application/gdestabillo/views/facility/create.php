a7ee79d1a067c6976639219a9e0ce37e2b44cb98
gabestabillo <tetotcommander@gmail.com>
2017-08-30 17:08:07 +0800

    Finalized Yii2.

--- Application/gdestabillo/views/facility/create.php
+++ Application/gdestabillo/views/facility/create.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 
 
 /* @var $this yii\web\View */
-/* @var $model app\models\facility */
+/* @var $model app\models\Facility */
 
 $this->title = 'Create Facility';
 $this->params['breadcrumbs'][] = ['label' => 'Facilities', 'url' => ['index']];

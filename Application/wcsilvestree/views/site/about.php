6ff5ba130a7ebd98eb9b2feb5d3b89618d571a84
alcornel <argeljanfred10@gmail.com>
2017-08-31 00:40:49 +0800

    Finalized Application/wcsilvestree/views/site/about.php

---
+++ Application/wcsilvestree/views/site/about.php
@@ -0,0 +1,74 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'H2H Application';
+?>
+<div class="site-about">
+
+    <div class="jumbotron">
+        <h1>About Us</h1>
+
+        <p class="lead">SM Hotels Facility Inspection Module</p>
+
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-3">
+                <?php echo Html::img('@web/1.png', ['width'=>'200'], ['height'=>'200']) ?>
+
+                <h2>Gabriel Estabillo</h2>
+                <h4>Project Manager</h4>
+
+                <p>Description</p>
+
+                <p><a class="btn btn-default" href="
+						https://www.facebook.com/gab.estabillo">Facebook &raquo;</a>
+                    <a class="btn btn-default" href="
+						https://www.facebook.com/gab.estabillo">Twitter &raquo;</a>
+            </div>
+            <div class="col-lg-3">
+                <?php echo Html::img('@web/2.png', ['width'=>'200'], ['height'=>'200']) ?>
+                <h2>Argel Cornel</h2>
+
+                <h4>Project Documentation</h4>
+
+                <p>Description</p>
+
+                <p><a class="btn btn-default" href="
+						https://www.facebook.com/gab.estabillo">Facebook &raquo;</a>
+                    <a class="btn btn-default" href="
+						https://www.facebook.com/gab.estabillo">Twitter &raquo;</a></p>
+            </div>
+            <div class="col-lg-3">
+                <?php echo Html::img('@web/3.png', ['width'=>'200'], ['height'=>'200']) ?>
+                <h2>Leo Salvador</h2>
+                <h4>Project Analyst</h4>
+                <p>Description</p>
+
+                <p><a class="btn btn-default" href="
+						https://www.facebook.com/gab.estabillo">Facebook &raquo;</a>
+                    <a class="btn btn-default" href="
+						https://www.facebook.com/gab.estabillo">Twitter &raquo;</a></p>
+            </div>
+            <div class="col-lg-3">
+                <?php echo Html::img('@web/4.png', ['width'=>'200'], ['height'=>'200']) ?>
+                <h2>Wesley Silvestre</h2>
+                <h4>Project Programmer</h4>
+
+                <p>Description</p>
+
+                <p><a class="btn btn-default" href="
+						https://www.facebook.com/gab.estabillo">Facebook &raquo;</a>
+                    <a class="btn btn-default" href="
+						https://www.facebook.com/gab.estabillo">Twitter &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
+

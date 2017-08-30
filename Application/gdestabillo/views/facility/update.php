c07ece61febadf9ba1def249e7de3395bceecc27
gabestabillo <tetotcommander@gmail.com>
2017-08-30 17:08:36 +0800

    Finalized Yii2.

--- Application/gdestabillo/views/facility/update.php
+++ Application/gdestabillo/views/facility/update.php
@@ -3,7 +3,7 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\facility */
+/* @var $model app\models\Facility */
 
 $this->title = 'Update Facility: ' . $model->id;
 $this->params['breadcrumbs'][] = ['label' => 'Facilities', 'url' => ['index']];

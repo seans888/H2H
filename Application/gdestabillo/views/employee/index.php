819ee22a2e12ddd4d43c8d4114152cc9faad857b
gabestabillo <tetotcommander@gmail.com>
2017-08-30 17:11:55 +0800

    Finalized Yii2.

---
+++ Application/gdestabillo/views/employee/index.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\EmployeeSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Employees';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="employee-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Employee', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            // 'id',
+            'employee_lastname',
+            'employee_firstname',
+            'employee_number',
+            'employee_email:email',
+            // 'employee_occupation',
+            // 'employee_user_type',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>

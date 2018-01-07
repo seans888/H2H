2688ff5995be3b28c55f8b0f5b80a438c4782cdd
wishix <silvestrewes@gmail.com>
2018-01-07 22:42:19 +0800

    Updated our System

--- Application/wcsilvestree/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=mi151_00_master_db',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
